c558790c30a0a56e3d19b7ed55580979b116857d
laura-bluz <laura.bluz@hotmail.com>
2022-06-26 20:50:04 -0300

    Operações +, -,  * e / feitas

--- src/components/Calc.jsx
+++ src/components/Calc.jsx
@@ -1,22 +1,67 @@
+import { useState } from "react";
+
+function enviar(estado, valor1, valor2) {
+    let resultado = 0;
+    console.log(estado, valor1, valor2)
+    switch (estado) {
+        case '+':
+            resultado = parseInt(valor1) + parseInt(valor2);
+            break;
+        case '-':
+            resultado = valor1 - valor2;
+            break;
+        case '*':
+            resultado = valor1 * valor2;
+            break;
+        case '/':
+            resultado = valor1 / valor2;
+            break;
+        default:
+            resultado = <p>Ainda não fiz %</p>
+    }
+    return resultado;
+}
 
 function Calc() {
-    return (<>
-        <h1>Selecione a operação desejada:</h1>
-        <select id="select" defaultValue={"1"}>
-            <option value="1">+</option>
-            <option value="2">-</option>
-            <option value="3">*</option>
-            <option value="4">/</option>
-            <option value="5">%</option>
-        </select>
+    const [estado, setEstado] = useState('+');
+    let [resultado, setResultado] = useState(0);
 
-        <br></br><br></br>
-        <div id="input"><input type='number'></input>
-            <h1>+</h1><input type='number'></input>
-            <h2>=</h2>
-            <p>Resultado</p>
-        </div>
-    </>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let form = new FormData(e.target);
+
+        const dados = Object.fromEntries(form);
+        console.log(dados);
+      
+        setResultado(enviar(dados.operacao, dados.valor1, dados.valor2));
+    }
+
+
+    return (
+        <>
+            <h1>Selecione a operação desejada:</h1>
+
+            <div id="input">
+                <form onSubmit={handleSubmit}>
+                    <select id="select" name='operacao' defaultValue={estado} onChange={e => setEstado(e.target.value)}>
+                        <option value="+">+</option>
+                        <option value="-">-</option>
+                        <option value="*">*</option>
+                        <option value="/">/</option>
+                        <option value="%">%</option>
+                    </select>
+
+                    <br></br><br></br>
+
+                    <input className='number' name='valor1' id='number1' type='text'></input>
+                    <h1>{estado}</h1>
+                    <input className='number' name='valor2' id='number2' type='number'></input>
+                    <br></br><br></br>
+                    <button type='submit'>=</button>
+                </form>
+                <p>{resultado}</p>
+            </div>
+        </>
     );
 }
 
